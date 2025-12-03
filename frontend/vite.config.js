b784b2b47babba5098dffceeae1f37a6a0124b4f
7078-cj <7078ceejay@gmail.com>
2025-12-03 21:54:27 +0800

    frontend deployment preparation

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss() ],
+export default defineConfig(({ mode }) => {
+  // load variables from .env files
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    server: {
+      allowedHosts: [env.VITE_HOST_URL], // 👈 use env here
+      host: '0.0.0.0',
+    },
+    plugins: [react(), tailwindcss()],
+     base: "/",
+  }
 })
