8c7e59320dbf48eac1883e36f177f00df2b781cd
rshdgvn <rasheedgavinesponga@gmail.com>
2025-12-03 14:49:55 +0800

    frontend cleanups

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,16 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
-import Login from './Pages/Login'
-import PrivateRoutes from './Contexts/PrivateRoutes'
-import Register from './Pages/Register'
-import { AuthProvider } from './Contexts/AuthContext'
-import DashBoard from './Pages/DashBoard'
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
-  
   return (
     <>
-      
       <Router>
-        <AuthProvider>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-              
-            <Route element={<PrivateRoutes />} >
-              <Route path="/" element={<DashBoard />} />
-            </Route>
-              
-          </Routes>
-        </AuthProvider>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
       </Router>
-      
-      
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
