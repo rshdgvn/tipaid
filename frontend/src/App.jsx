a81c15a4346a1fc59f86bda86fdb238fff2e343e
rshdgvn <rasheedgavinesponga@gmail.com>
2025-12-04 04:47:22 +0800

    cleanups

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import FormPage from "./pages/FormPage";
-import SummaryPage from "./pages/SummaryPage";
 import RecipeResultsPage from "./pages/RecipeResultsPage";
 import RecommendationResultsPage from "./pages/RecommendationResultsPage";
 import Layout from "./layouts/Layout";
@@ -15,7 +14,6 @@ export default function App() {
           <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/form" element={<FormPage />} />
-            <Route path="/summary" element={<SummaryPage />} />
             <Route path="/recipe-results" element={<RecipeResultsPage />} />
             <Route
               path="/recommendation"
